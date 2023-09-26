0833194c82c9ffbe2e42302faf8a252995ef89af
Shivangi varshney <132803861+Shivangii13@users.noreply.github.com>
2023-09-26 18:56:18 +0530

    initial commit

---
+++ strings.py
@@ -0,0 +1,4 @@
+print("Day 1 - string manipulation")
+print('String Concatination is done with the "+" sign.')
+print('e.g. print("Hello" + "world")')
+print("New lines can be created with a backlash and n.")
